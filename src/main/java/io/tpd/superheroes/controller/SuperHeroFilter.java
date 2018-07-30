89c595292c97b53997c46111665ca5705853315c
cloro87 <32708705+cloro87@users.noreply.github.com>
2018-07-30 20:53:12 +0100

    added travis

--- src/main/java/io/tpd/superheroes/controller/SuperHeroFilter.java
+++ src/main/java/io/tpd/superheroes/controller/SuperHeroFilter.java
@@ -1,10 +1,14 @@
 package io.tpd.superheroes.controller;
 
-import org.springframework.stereotype.Component;
-
-import javax.servlet.*;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
 
 /**
  * @author moises.macero
@@ -12,16 +16,17 @@ import java.io.IOException;
 @Component
 public class SuperHeroFilter implements Filter {
 
-    @Override
-    public void init(FilterConfig filterConfig) {}
+  @Override
+  public void init(FilterConfig filterConfig) {}
 
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        var httpServletResponse = (HttpServletResponse) servletResponse;
-        httpServletResponse.setHeader("X-SUPERHERO-APP", "super-header");
-        filterChain.doFilter(servletRequest, servletResponse);
-    }
+  @Override
+  public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
+      FilterChain filterChain) throws IOException, ServletException {
+    HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
+    httpServletResponse.setHeader("X-SUPERHERO-APP", "super-header");
+    filterChain.doFilter(servletRequest, servletResponse);
+  }
 
-    @Override
-    public void destroy() {}
+  @Override
+  public void destroy() {}
 }
